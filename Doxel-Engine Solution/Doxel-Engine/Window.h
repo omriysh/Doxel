af37db5af47c3eb00c9726fe02ab932cdf3d06f9
Nitzan Elbaz <nitzanel2@gmail.com>
2015-09-09 17:24:21 +0300

    Added Window system
    Added Window.cpp and finished Window.h
    Also added SimpleErrors.h.

--- Doxel-Engine Solution/Doxel-Engine/Window.h
+++ Doxel-Engine Solution/Doxel-Engine/Window.h
@@ -1,23 +1,35 @@
 #pragma once
 #include <GLFW\glfw3.h>
 
-// #define Window(Class)
 
-Class Window()
+
+#define DEFAULT_TITLE "Doxel Engine"
+#define DEFAULT_WINDOW_WIDTH 800
+#define DEFAULT_WINDOW_HEIGHT 600
+
+
+class Window
 {
 public:
-Window(); //  empty
-~Window(); // empty
+Window(); ///<  empty
+~Window(); ///< empty
 
-void init(int width, int height, char* title); // Init the window
-void destroy(); // Destroy the window
+bool init(int width , int height , char* title = DEFAULT_TITLE); // Init the window, will return true if successful
+bool isInitialized() { return m_window != nullptr; }
+
+void destroy(); ///< Destroy the window
+
+void update(); ///< to be called at the end of every frame
+
+void setWindowClose();
+bool shouldWindowClose();
 
 private:
-glfwWindow* m_window = nullptr;
+GLFWwindow* m_window = nullptr;
 int m_width, m_height;
 char* m_title;
 
 
 
 
-}
+};
